3541a684442bbf744faaacd345d23902a843e402
fmwctodev <srichard358@gmail.com>
2026-01-01 19:07:44 -0500

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,21 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 5174,
-    strictPort: true,   // 👈 Forces Vite to use this exact port
+    strictPort: true,
   },
   optimizeDeps: {
     exclude: ['lucide-react'],
     include: ['html2pdf.js'],
+  },
+  build: {
+    commonjsOptions: {
+      include: [/html2pdf\.js/, /node_modules/],
+      transformMixedEsModules: true,
+    },
+  },
+  resolve: {
+    alias: {
+      'html2pdf.js': 'html2pdf.js/dist/html2pdf.bundle.min.js',
+    },
   },
 })
