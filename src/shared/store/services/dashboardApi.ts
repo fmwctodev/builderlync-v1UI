d396ae485988fd5b07b080c60f5604f4d96335fd
sandeepsheoran1234 <sheoransandeep151@gmail.com>
2026-02-24 21:07:53 +0530

    dashboard stats

--- src/shared/store/services/dashboardApi.ts
+++ src/shared/store/services/dashboardApi.ts
@@ -21,7 +21,7 @@ const baseQueryWithReauth: BaseQueryFn<
   FetchBaseQueryError
 > = async (args, api, extraOptions) => {
   let result = await baseQuery(args, api, extraOptions);
-  if (result.error && (result.error.status === 401 || result.error.status === 403)) {
+  if (result.error && (result.error.status === 403)) {
     logoutAndRedirect();
   }
   return result;
